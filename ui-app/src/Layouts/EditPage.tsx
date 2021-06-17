dac06a5b8015f035dd4c0b29164e3c4dd97bf4e7
Cristian Mejia <cristianmejia97@gmail.com>
2021-06-17 10:30:35 -0400

    add page header

--- ui-app/src/Layouts/EditPage.tsx
+++ ui-app/src/Layouts/EditPage.tsx
@@ -1,6 +1,6 @@
 import React, { FC, useState } from 'react';
 import { Link } from 'react-router-dom';
-import { Layout, Menu, Breadcrumb } from 'antd';
+import { Layout, Menu, Breadcrumb, PageHeader, Button } from 'antd';
 import { MenuUnfoldOutlined, MenuFoldOutlined } from '@ant-design/icons';
 import AdminHeader from '../Components/AdminHeader';
 
@@ -10,6 +10,20 @@ type Props = {
   left?: React.ReactNode,
   children?: React.ReactNode
 }
+const routes = [
+  {
+    path: '/',
+    breadcrumbName: 'Home',
+  },
+  {
+    path: '/product-list',
+    breadcrumbName: 'Items',
+  },
+  {
+    path: '/item/id',
+    breadcrumbName: 'Item Name',
+  },
+];
 
 const EditPageLayout: FC<Props> = (props: Props) => {
   const [collapsed, setCollapsed] = useState(false);
@@ -17,25 +31,26 @@ const EditPageLayout: FC<Props> = (props: Props) => {
   return (
     <Layout>
       <AdminHeader />
+        <PageHeader
+          breadcrumb={{ routes }}
+          ghost={false}
+          backIcon={collapsed ? <MenuUnfoldOutlined/> :<MenuFoldOutlined />}
+          onBack={() => setCollapsed(!collapsed)}
+          title="Title"
+          subTitle="This is a subtitle"
+          extra={[
+            <Button key="2">Discard</Button>,
+            <Button key="1" type="primary">
+              Save
+            </Button>,
+          ]}
+        >
+        </PageHeader>
       <Content style={{ padding: '0 50px' }}>
-        <Breadcrumb style={{ margin: '16px 0' }}>
-          <Breadcrumb.Item>
-            <Link to="/">Home</Link>
-          </Breadcrumb.Item>
-          <Breadcrumb.Item>
-            <Link to="/product-list">Products</Link>
-          </Breadcrumb.Item>
-          <Breadcrumb.Item>
-            Item
-          </Breadcrumb.Item>
-        </Breadcrumb>
-        {React.createElement(collapsed ? MenuUnfoldOutlined : MenuFoldOutlined, {
-          className: 'trigger',
-          onClick: () => setCollapsed(!collapsed)
-        })}
-        <Layout className="site-layout-background" style={{ padding: '24px 0' }}>
+        <Layout className="" style={{ padding: '24px 0' }}>
           <Sider
-            className="site-layout-background"
+            style={{ background: '#fff' }}
+            className="gray-2"
             trigger={null}
             collapsible
             collapsed={collapsed}
